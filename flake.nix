58238e9ec36de036171a1a975d071ab53f97d1f2
matthewcroughan <matt@croughan.sh>
2022-01-17 14:41:07 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     cardano-node = {
-      url = "github:input-output-hk/cardano-node/1.31.0";
+      url = "github:input-output-hk/cardano-node/1.33.0";
     };
     flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
     flake-compat = {
@@ -53,26 +53,127 @@
               test = makeTest {
                 name = "cardano-testnet";
                 nodes = {
-                  client = { config, pkgs, ... }: {
+                  producer = { config, pkgs, ... }: {
+                    networking.firewall.enable = false;
+                    environment.systemPackages =
+                      [ 
+                        cardano-node.packages."${system}".cardano-cli
+                        cardano-node.packages."${system}".cardano-node
+                        pkgs.tree
+                      ];
                     imports = [ cardano-node.nixosModules.cardano-node ];
                     services.cardano-node = {
+                      hostAddr = "0.0.0.0";
                       enable = true;
+                      topology = "${./data/topology-producer.json}";
+                      #producers = lib.mkOverride [
+                      #  {
+                      #    addrs = [{ addr = "127.0.0.1"; port = 12799; }];
+                      #    advertise = false;
+                      #    valency = 1;
+                      #  }
+                      #];
+                      nodeConfigFile = "${./config.json}";
+                      vrfKey = "/tmp/genesis/delegate-keys/delegate1.vrf.skey";
+                      kesKey = "/tmp/genesis/delegate-keys/delegate1.kes.skey";
+                      operationalCertificate =
+                        "/tmp/genesis/delegate-keys/opcert1.cert";
                       forceHardForks = {
                         shelley = 0;
                         allegra = 0;
                         mary    = 0;
                         alonzo  = 0;
                       };
-                    extraNodeConfig = {};
+                    };
+                  };
+                  relay = { config, pkgs, ... }: {
+                    networking.firewall.enable = false;
+                    environment.systemPackages =
+                      [ 
+                        cardano-node.packages."${system}".cardano-cli
+                        cardano-node.packages."${system}".cardano-node
+                        pkgs.tree
+                      ];
+                    imports = [ cardano-node.nixosModules.cardano-node ];
+                    services.cardano-node = {
+                      hostAddr = "0.0.0.0";
+                      enable = true;
+                      topology = "${./data/topology-relay.json}";
+                      #producers = lib.mkOverride [
+                      #  {
+                      #    addrs = [{ addr = "127.0.0.1"; port = 12799; }];
+                      #    adhostAddrvertise = false;
+                      #    valency = 1;
+                      #  }
+                      #];
+                      nodeConfigFile = "${./config.json}";
+                      forceHardForks = {
+                        shelley = 0;
+                        allegra = 0;
+                        mary    = 0;
+                        alonzo  = 0;
+                      };
                     };
                   };
                 };
 
-                testScript = ''
+                testScript =
+                  let
+                    generateEverything = pkgs.writeScript "generateEverything.sh" ''
+
+                      cp -R "${./genesis}" ./genesis
+                      cp -R "${./genesis-byron}" ./genesis-byron
+                      chmod -R 700 genesis
+                      chmod -R 700 genesis-byron
+
+                      cardano-cli genesis create \
+                        --genesis-dir genesis/ \
+                        --supply 1000000000 \
+                        --gen-genesis-keys 1 \
+                        --gen-utxo-keys 1 \
+                        --testnet-magic 42
+
+                      cardano-cli byron genesis genesis \
+                        --protocol-magic 42 \
+                        --start-time $(date +%s --date="10 seconds") \
+                        --k 10 \
+                        --n-poor-addresses 0 \
+                        --n-delegate-addresses 1 \
+                        --total-balance 1000000000 \
+                        --delegate-share 1 \
+                        --avvm-entry-count 0 \
+                        --avvm-entry-balance 0 \
+                        --protocol-parameters-file "${./data/byron.genesis.spec.json}" \
+                        --genesis-output-dir genesis-byron/
+
+                      cp "${./genesis/genesis.json}" ./genesis
+                      cp "${./genesis/genesis.alonzo.json}" ./genesis
+                      cp "${./genesis-byron/genesis.json}" ./genesis-byron
+
+                      chmod -R 700 genesis
+                      chmod -R 700 genesis-byron
+                      chown -R cardano-node:cardano-node genesis
+                      chown -R cardano-node:cardano-node genesis-byron
+                      cat ./genesis/genesis.json
+                      cat ./genesis-byron/genesis.json
+                      tree genesis
+                      tree genesis-byron
+                      find ./genesis -type f -exec sha256sum {} \;
+                      find ./genesis-byron -type f -exec sha256sum {} \;
+                    '';
+                  in
+                ''
                   start_all()
-                  client.succeed("echo hello")
-                  client.wait_for_unit("cardano-node.service")
-                  client.wait_for_open_port("3001")
+                  producer.succeed("${generateEverything} >&2")
+                  relay.succeed("${generateEverything} >&2")
+                  producer.wait_for_unit("cardano-node.service")
+                  producer.wait_for_open_port("3001")
+                  relay.wait_for_unit("cardano-node.service")
+                  relay.wait_for_open_port("3001")
+                  relay.succeed("ifconfig >&2")
+                  relay.sleep("600")
+                  producer.succeed("ifconfig >&2")
+                  producer.sleep("600")
                 '';
               };
             in test;
